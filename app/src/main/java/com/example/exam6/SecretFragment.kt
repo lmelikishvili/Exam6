fcc218146b8fa1d084cffb22729b0a1bbe8ba95c
lmelikishvili <levanimelikishvili@gmail.com>
2023-12-27 10:07:34 +0400

    base setup

---
+++ app/src/main/java/com/example/exam6/SecretFragment.kt
@@ -0,0 +1,24 @@
+package com.example.exam6
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.exam6.databinding.FragmentSecretBinding
+
+
+class SecretFragment : BaseFragment<FragmentSecretBinding>(FragmentSecretBinding::inflate) {
+    override fun setup() {
+
+    }
+
+    override fun setupListeners() {
+
+    }
+
+    override fun bindData() {
+
+    }
+
+}
